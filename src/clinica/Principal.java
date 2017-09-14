1551fed222accb330cc82999f47160f0f4778925
mapoggi <mapoggi@fibertel.com.ar>
2017-09-14 10:46:34 -0300

    correccion de clinica arranque programa

--- src/clinica/Principal.java
+++ src/clinica/Principal.java
@@ -5,15 +5,22 @@
  */
 package clinica;
 
-import visual.Jinicio;
-import visual.Jlogin;
-import visual.Jpersona;
-<<<<<<< HEAD
-import visual.bienvenido;
-import visual.menuprincipal;
-=======
+
+import controladorInicio.ControladorRegistrarse;
+import controladorInicio.ControladorSolicitudTurno;
+import controladorInicio.ControladorTurnosDisponibles;
+import controladorInicio.ControladorWindowsInicio;
+import java.awt.GridLayout;
+import java.awt.event.MouseListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 import visual.Bienvenido;
->>>>>>> 401ef9684490f0c70ee9c8818ab958a615eb2da5
+import visual.MenuPrincipal;
+
+import visual.Bienvenido;
+
 
 /**
  *
@@ -22,17 +29,14 @@ import visual.Bienvenido;
 public class Principal {
     public static void main(String[] args) {
        //Jlogin login = new Jlogin();
-<<<<<<< HEAD
+
      //   Jpersona persona = new Jpersona();
-=======
+
         Bienvenido holaMundo = new Bienvenido();
         holaMundo.setVisible(true);
         holaMundo.setAlwaysOnTop(true);
->>>>>>> 401ef9684490f0c70ee9c8818ab958a615eb2da5
-       // Jinicio inicio = new Jinicio();
-     //  new menuprincipal().setVisible(true);
-         bienvenido menu = new bienvenido();
-         menu.setVisible(true);
+
+
     }
     
 }
