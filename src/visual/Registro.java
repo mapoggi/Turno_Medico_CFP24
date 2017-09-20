9f4420e17ddc1aee72b56876049c6d559f737ace
Your Name <maratial@gmail.com>
2017-09-19 21:14:57 -0300

    Modifico base de datos. Registro . Especialidad.

--- src/visual/Registro.java
+++ src/visual/Registro.java
@@ -5,6 +5,9 @@
  */
 package visual;
 
+import conroladores.ControladorRegistrar;
+import java.awt.event.MouseListener;
+
 /**
  *
  * @author Silvia
@@ -16,9 +19,41 @@ public class Registro extends javax.swing.JPanel {
      */
     public Registro() {
         initComponents();
-        this.setLocation(300, 300);
+        this.setLocation(null);
+        MouseListener controladorRegistrar = new ControladorRegistrar(textUsu, textClave, textClaveRep,textEmail);
+        botReg.addMouseListener(controladorRegistrar);
     }
 
+        public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ProfRegistro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ProfRegistro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ProfRegistro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ProfRegistro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            new Registro().setVisible(true);
+        });
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -28,18 +63,34 @@ public class Registro extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
+        textUsu = new javax.swing.JTextField();
+        textClave = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
+        textClaveRep = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
-        jTextField4 = new javax.swing.JTextField();
-        jButton5 = new javax.swing.JButton();
+        botReg = new javax.swing.JButton();
+        textEmail = new javax.swing.JTextField();
+        botInicio = new javax.swing.JButton();
         jLabel7 = new javax.swing.JLabel();
+
+        jButton1.setText("jButton1");
+
+        jButton2.setText("jButton2");
+
+        jButton3.setText("jButton3");
+
+        jButton4.setText("jButton4");
+
+        jButton5.setText("jButton5");
 
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
@@ -59,36 +110,47 @@ public class Registro extends javax.swing.JPanel {
         jLabel4.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel4.setText("Repita la contraseña:");
         add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 250, 170, -1));
-        add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 170, 320, -1));
-        add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 210, 320, -1));
+        add(textUsu, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 170, 320, -1));
+        add(textClave, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 210, 320, -1));
 
         jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/visual/imagenes/images (1).jpg"))); // NOI18N
         add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 330, 110, 100));
-        add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 250, 320, -1));
+        add(textClaveRep, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 250, 320, -1));
 
         jLabel5.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel5.setText("email de seguridad:");
         add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 290, 140, -1));
-        add(jTextField4, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 290, 320, -1));
 
-        jButton5.setText("Volver a inicio");
-        jButton5.addActionListener(new java.awt.event.ActionListener() {
+        botReg.setText("Registrar");
+        add(botReg, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 380, 100, -1));
+        add(textEmail, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 290, 320, -1));
+
+        botInicio.setText("Volver a inicio");
+        botInicio.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton5ActionPerformed(evt);
+                botInicioActionPerformed(evt);
             }
         });
-        add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 380, -1, -1));
+        add(botInicio, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 380, -1, -1));
 
         jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/visual/imagenes/Say-Goodbye-to-Coworkers-Step-8-Version-2.jpg"))); // NOI18N
-        add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 720, 430));
+        add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 730, 420));
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton5ActionPerformed
+    private void botInicioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botInicioActionPerformed
+               MenuPrincipal jFrame = new MenuPrincipal ();
+               jFrame.setVisible (true);
+               
+    }//GEN-LAST:event_botInicioActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton botInicio;
+    private javax.swing.JButton botReg;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -97,9 +159,9 @@ public class Registro extends javax.swing.JPanel {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTextField textClave;
+    private javax.swing.JTextField textClaveRep;
+    private javax.swing.JTextField textEmail;
+    private javax.swing.JTextField textUsu;
     // End of variables declaration//GEN-END:variables
 }
